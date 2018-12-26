478f0144845bcbd37ea9f149a48c6d077f74c23d
xq262144 <xq262144@gmail.com>
2018-12-26 14:19:41 +0800

    Runnable version using CovenantSQL

--- src/main/resources/db/migration/V1.0.0.01__create_schema.sql
+++ src/main/resources/db/migration/V1.0.0.01__create_schema.sql
@@ -1,5 +1,5 @@
 --
---    Copyright 2016 the original author or authors.
+--    Copyright 2016-2018 the original author or authors.
 --
 --    Licensed under the Apache License, Version 2.0 (the "License");
 --    you may not use this file except in compliance with the License.
@@ -16,149 +16,143 @@
 
 create table supplier (
     suppid int not null,
-    name varchar(80) null,
-    status varchar(2) not null,
-    addr1 varchar(80) null,
-    addr2 varchar(80) null,
-    city varchar(80) null,
-    state varchar(80) null,
-    zip varchar(5) null,
-    phone varchar(80) null,
-    constraint pk_supplier primary key (suppid)
+    name text,
+    status text not null,
+    addr1 text,
+    addr2 text,
+    city text,
+    state text,
+    zip text,
+    phone text,
+    primary key (suppid)
 );
 
 create table signon (
-    username varchar(25) not null,
-    password varchar(80)  not null,
-    constraint pk_signon primary key (username)
+    username text not null,
+    password text not null,
+    primary key (username)
 );
 
 create table account (
-    userid varchar(80) not null,
-    email varchar(80) not null,
-    firstname varchar(80) not null,
-    lastname varchar(80) not null,
-    status varchar(2)  null,
-    addr1 varchar(80) not null,
-    addr2 varchar(40) null,
-    city varchar(80) not  null,
-    state varchar(80) not null,
-    zip varchar(20) not null,
-    country varchar(20) not null,
-    phone varchar(80) not null,
-    constraint pk_account primary key (userid)
+    userid text not null,
+    email text not null,
+    firstname text not null,
+    lastname text not null,
+    status text,
+    addr1 text not null,
+    addr2 text,
+    city text not null,
+    state text not null,
+    zip text not null,
+    country text not null,
+    phone text not null,
+    primary key (userid)
 );
 
 create table profile (
-    userid varchar(80) not null,
-    langpref varchar(80) not null,
-    favcategory varchar(30),
+    userid text not null,
+    langpref text not null,
+    favcategory text,
     mylistopt int,
     banneropt int,
-    constraint pk_profile primary key (userid)
+    primary key (userid)
 );
 
 create table bannerdata (
-    favcategory varchar(80) not null,
-    bannername varchar(255)  null,
-    constraint pk_bannerdata primary key (favcategory)
+    favcategory text not null,
+    bannername text,
+    primary key (favcategory)
 );
 
 create table orders (
     orderid int not null,
-    userid varchar(80) not null,
+    userid text not null,
     orderdate timestamp not null,
-    shipaddr1 varchar(80) not null,
-    shipaddr2 varchar(80) null,
-    shipcity varchar(80) not null,
-    shipstate varchar(80) not null,
-    shipzip varchar(20) not null,
-    shipcountry varchar(20) not null,
-    billaddr1 varchar(80) not null,
-    billaddr2 varchar(80)  null,
-    billcity varchar(80) not null,
-    billstate varchar(80) not null,
-    billzip varchar(20) not null,
-    billcountry varchar(20) not null,
-    courier varchar(80) not null,
-    totalprice decimal(10,2) not null,
-    billtofirstname varchar(80) not null,
-    billtolastname varchar(80) not null,
-    shiptofirstname varchar(80) not null,
-    shiptolastname varchar(80) not null,
-    creditcard varchar(80) not null,
-    exprdate varchar(7) not null,
-    cardtype varchar(80) not null,
-    locale varchar(80) not null,
-    constraint pk_orders primary key (orderid)
+    shipaddr1 text not null,
+    shipaddr2 text,
+    shipcity text not null,
+    shipstate text not null,
+    shipzip text not null,
+    shipcountry text not null,
+    billaddr1 text not null,
+    billaddr2 text,
+    billcity text not null,
+    billstate text not null,
+    billzip text not null,
+    billcountry text not null,
+    courier text not null,
+    totalprice text not null,
+    billtofirstname text not null,
+    billtolastname text not null,
+    shiptofirstname text not null,
+    shiptolastname text not null,
+    creditcard text not null,
+    exprdate text not null,
+    cardtype text not null,
+    locale text not null,
+    primary key (orderid)
 );
 
 create table orderstatus (
     orderid int not null,
     linenum int not null,
     timestamp date not null,
-    status varchar(2) not null,
-    constraint pk_orderstatus primary key (orderid, linenum)
+    status text not null,
+    primary key (orderid, linenum)
 );
 
 create table orderlines (
     orderid int not null,
     linenum int not null,
-    itemid varchar(10) not null,
+    itemid text not null,
     quantity int not null,
-    unitprice decimal(10,2) not null,
-    constraint pk_lineitem primary key (orderid, linenum)
+    unitprice text not null,
+    primary key (orderid, linenum)
 );
 
 create table category (
-    catid varchar(10) not null,
-    name varchar(80) null,
-    descn varchar(255) null,
-    constraint pk_category primary key (catid)
+    catid text not null,
+    name text,
+    descn text,
+    primary key (catid)
 );
 
 create table product (
-    productid varchar(10) not null,
-    category varchar(10) not null,
-    name varchar(80) null,
-    descn varchar(255) null,
-    constraint pk_product primary key (productid),
-        constraint fk_product_1 foreign key (category)
-        references category (catid)
+    productid text not null,
+    category text not null,
+    name text,
+    descn text,
+    primary key (productid)
 );
 
 create index productCat on product (category);
 create index productName on product (name);
 
 create table item (
-    itemid varchar(10) not null,
-    productid varchar(10) not null,
-    listprice decimal(10,2) null,
-    unitcost decimal(10,2) null,
-    supplier int null,
-    status varchar(2) null,
-    attr1 varchar(80) null,
-    attr2 varchar(80) null,
-    attr3 varchar(80) null,
-    attr4 varchar(80) null,
-    attr5 varchar(80) null,
-    constraint pk_item primary key (itemid),
-        constraint fk_item_1 foreign key (productid)
-        references product (productid),
-        constraint fk_item_2 foreign key (supplier)
-        references supplier (suppid)
+    itemid text not null,
+    productid text not null,
+    listprice text,
+    unitcost text,
+    supplier int,
+    status text,
+    attr1 text,
+    attr2 text,
+    attr3 text,
+    attr4 text,
+    attr5 text,
+    primary key (itemid)
 );
 
 create index itemProd on item (productid);
 
 create table inventory (
-    itemid varchar(10) not null,
+    itemid text not null,
     qty int not null,
-    constraint pk_inventory primary key (itemid)
+    primary key (itemid)
 );
 
 CREATE TABLE sequence(
-    name               varchar(30)  not null,
-    nextid             int          not null,
-    constraint pk_sequence primary key (name)
+    name text not null,
+    nextid int not null,
+    primary key (name)
 );
--- src/main/resources/db/migration/V1.0.0.02__import_data.sql
+++ src/main/resources/db/migration/V1.0.0.02__import_data.sql
@@ -1,5 +1,5 @@
 --
---    Copyright 2016 the original author or authors.
+--    Copyright 2016-2018 the original author or authors.
 --
 --    Licensed under the Apache License, Version 2.0 (the "License");
 --    you may not use this file except in compliance with the License.
@@ -115,5 +115,3 @@ INSERT INTO inventory (itemid, qty ) VALUES ('EST-25',10000);
 INSERT INTO inventory (itemid, qty ) VALUES ('EST-26',10000);
 INSERT INTO inventory (itemid, qty ) VALUES ('EST-27',10000);
 INSERT INTO inventory (itemid, qty ) VALUES ('EST-28',10000);
-
-commit;
